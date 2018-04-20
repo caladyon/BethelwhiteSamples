1dff0ddef857d0116369b71914b4aa87c9402961
caladyon <caladyon@gmail.com>
2018-04-20 08:34:00 +0200

    Creation of delombokfullness
    Just a simple project: pom, class without lombok annotations, class with
    lombok annotation.

---
+++ delombokfullness/src/main/java/it/caladyon/delombokfullness/VanillaClass.java
@@ -0,0 +1,52 @@
+/**
+ * 
+ */
+package it.caladyon.delombokfullness;
+
+/**
+ * Class with one integer field, and boilerplate methods.
+ * 
+ * @author luciano.boschi
+ *
+ */
+public class VanillaClass {
+
+    private final int value;
+
+    public VanillaClass(int value) {
+	super();
+	this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    @Override
+    public int hashCode() {
+	final int prime = 31;
+	int result = 1;
+	result = prime * result + value;
+	return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+	if (this == obj)
+	    return true;
+	if (obj == null)
+	    return false;
+	if (getClass() != obj.getClass())
+	    return false;
+	VanillaClass other = (VanillaClass) obj;
+	if (value != other.value)
+	    return false;
+	return true;
+    }
+
+    @Override
+    public String toString() {
+	return "VanillaClass [value=" + value + "]";
+    }
+    
+}
---
+++ delombokfullness/src/main/lombok/it/caladyon/delombokfullness/LombokAnnotatedClass.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package it.caladyon.delombokfullness;
+
+import lombok.Data;
+
+/**
+ * Class with one integer field, and <code>@Data</code> annotation.
+ * 
+ * @author luciano.boschi
+ *
+ */
+@Data
+public class LombokAnnotatedClass {
+
+    private final int value;
+
+}
